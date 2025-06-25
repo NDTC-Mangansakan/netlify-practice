fffa5b53a236ba82b9b5ab4496f71d51fec1dc36
Shaq <mangansakansansudin@gmail.com>
2025-06-25 16:07:47 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import img from './assets/Images/destination-img-1.png'
+
+const App = () => {
+  return (
+    <div>
+      <img src={img} alt="" />
+      dkjasdhjshdj
+    </div>
+  )
+}
+
+export default App
