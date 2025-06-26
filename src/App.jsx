afa70b66791ac6fa431d4350f1105c91bb3939ca
Shaq <mangansakansansudin@gmail.com>
2025-06-26 16:41:21 +0800

    chnaged img directory-4

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import img from './assets/Images/destination-img-1.png'
+import img1 from '../public/Images/destination-img-1.png'
+import img2 from '../public/Images/destination-img-2.png'
+import img3 from '../public/Images/destination-img-3.png'
 
 const App = () => {
   return (
-    <div>
-      <img src={img} alt="" />
+    <div style={{
+      display: 'grid',
+      gridTemplateColumns: '200px 300px 200px'
+    }}>
+      <img src={img1} alt="" />
+      <img src={img2} alt="" />
+      <img src={img3} alt="" />
     </div>
   )
 }
